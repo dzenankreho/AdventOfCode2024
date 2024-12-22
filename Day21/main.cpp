899083dbb858bf65ad2715ed4b1f5f0e4f6989e6
Dženan Kreho <92307873+dzenankreho@users.noreply.github.com>
2024-12-22 15:31:44 +0100

    Add day 21 combined part 1 and 2 solution

--- Day21/main.cpp
+++ Day21/main.cpp
@@ -4,7 +4,8 @@
 #include <chrono>
 #include <string>
 #include <unordered_map>
-#include <limits>
+#include <utility>
+#include <cmath>
 
 
 #define MY_INPUT_PATH R"(..\inputDay21.txt)"
@@ -16,74 +17,101 @@ struct Position {
     int x;
     int y;
 
-    Position operator+(const Position& position) const {
-        return { x + position.x, y + position.y };
-    }
-
     Position operator-(const Position& position) const {
         return { x - position.x, y - position.y };
-    }
-
-    bool operator==(const Position& position) const {
-        return x == position.x && y == position.y;
     }
 };
 
 
-int solutionPart1(const char* inputPath) {
+template <>
+struct std::hash<std::pair<std::string, int>> {
+    std::size_t operator()(const std::pair<std::string, int>& pair) const {
+        return std::hash<std::string>()(pair.first) ^ (std::hash<int>()(pair.second) << 1);
+    }
+};
+
+
+using KeypadTransitionTable = std::unordered_map<char, std::unordered_map<char, std::vector<std::string>>>;
+
+
+long long countNumOfHumanButtonPresses(
+    const KeypadTransitionTable& directionalKeypadTransitions,
+    const std::string& currentDirKeypadInput,
+    const int numOfRobotsUsingDirKeypads,
+    std::unordered_map<std::pair<std::string, int>, long long>& inputCache
+) {
+    std::pair<std::string, int> input{ currentDirKeypadInput, numOfRobotsUsingDirKeypads };
+
+    if (inputCache.contains(input)) {
+        return inputCache.at(input);
+    }
+
+    long long numOfButtonPresses{};
+    char currentDirKeypadPosition{ 'A' };
+
+    if (numOfRobotsUsingDirKeypads == 1) {
+        for (char dirKeypadButton : currentDirKeypadInput) {
+            numOfButtonPresses +=
+                directionalKeypadTransitions.at(currentDirKeypadPosition).at(dirKeypadButton).front().length();
+
+            currentDirKeypadPosition = dirKeypadButton;
+        }
+    } else {
+        for (char dirKeypadButton : currentDirKeypadInput) {
+            numOfButtonPresses += countNumOfHumanButtonPresses(
+                directionalKeypadTransitions,
+                directionalKeypadTransitions.at(currentDirKeypadPosition).at(dirKeypadButton).front(),
+                numOfRobotsUsingDirKeypads - 1,
+                inputCache
+            );
+
+            currentDirKeypadPosition = dirKeypadButton;
+        }
+    }
+
+    inputCache.emplace(input, numOfButtonPresses);
+    return numOfButtonPresses;
+}
+
+
+long long solution(const char* inputPath, const int numOfRobotsUsingDirKeypads) {
     std::ifstream input(inputPath);
 
     std::unordered_map<char, Position> numericKeypad{
-                           { '0', { 3, 1 } }, { 'A', { 3, 2 } },
-        { '1', { 2, 0 } }, { '2', { 2, 1 } }, { '3', { 2, 2 } },
-        { '4', { 1, 0 } }, { '5', { 1, 1 } }, { '6', { 1, 2 } },
-        { '7', { 0, 0 } }, { '8', { 0, 1 } }, { '9', { 0, 2 } }
+            { '0', { 3, 1 } }, { 'A', { 3, 2 } },
+            { '1', { 2, 0 } }, { '2', { 2, 1 } }, { '3', { 2, 2 } },
+            { '4', { 1, 0 } }, { '5', { 1, 1 } }, { '6', { 1, 2 } },
+            { '7', { 0, 0 } }, { '8', { 0, 1 } }, { '9', { 0, 2 } }
     };
 
-    std::unordered_map<char, std::unordered_map<char, std::vector<std::string>>> numericKeypadTransitions;
+    KeypadTransitionTable numericKeypadTransitions;
     for (const auto& [fromValue, fromPosition] : numericKeypad) {
         std::unordered_map<char, std::vector<std::string>> transitions;
         for (const auto& [toValue, toPosition] : numericKeypad) {
-            std::vector<std::string> possiblePreviousRobotInputs;
-
             Position diff{ fromPosition - toPosition };
 
-            std::string possiblePrevInput1;
-            if (diff.x >= 0) {
-                possiblePrevInput1 += std::string(diff.x, '^');
-            } else {
-                possiblePrevInput1 += std::string(-diff.x, 'v');
-            }
-            if (diff.y >= 0) {
-                possiblePrevInput1 += std::string(diff.y, '<');
-            } else {
-                possiblePrevInput1 += std::string(-diff.y, '>');
-            }
-            possiblePrevInput1 += 'A';
+            std::string possiblePreviousDirKeypadInput1{
+                std::string(std::abs(diff.x), ((diff.x >= 0) ? ('^') : ('v'))) +
+                std::string(std::abs(diff.y), ((diff.y >= 0) ? ('<') : ('>')))
+            };
+            std::string possiblePreviousDirKeypadInput2{
+                possiblePreviousDirKeypadInput1.rbegin(), possiblePreviousDirKeypadInput1.rend()
+            };
 
-            std::string possiblePrevInput2;
-            if (diff.y >= 0) {
-                possiblePrevInput2 += std::string(diff.y, '<');
-            } else {
-                possiblePrevInput2 += std::string(-diff.y, '>');
-            }
-            if (diff.x >= 0) {
-                possiblePrevInput2 += std::string(diff.x, '^');
-            } else {
-                possiblePrevInput2 += std::string(-diff.x, 'v');
-            }
-            possiblePrevInput2 += 'A';
+            possiblePreviousDirKeypadInput1 += 'A';
+            possiblePreviousDirKeypadInput2 += 'A';
 
+            std::vector<std::string> possiblePreviousDirKeypadInputs;
             if (diff.x == 0 || diff.y == 0 || (diff.x > 0 && fromPosition.x == 3 && toPosition.y == 0)) {
-                possiblePreviousRobotInputs.push_back(possiblePrevInput1);
+                possiblePreviousDirKeypadInputs.push_back(possiblePreviousDirKeypadInput1);
             } else if (diff.x < 0 && fromPosition.y == 0 && toPosition.x == 3) {
-                possiblePreviousRobotInputs.push_back(possiblePrevInput2);
+                possiblePreviousDirKeypadInputs.push_back(possiblePreviousDirKeypadInput2);
             } else {
-                possiblePreviousRobotInputs.push_back(possiblePrevInput1);
-                possiblePreviousRobotInputs.push_back(possiblePrevInput2);
+                possiblePreviousDirKeypadInputs.push_back(possiblePreviousDirKeypadInput1);
+                possiblePreviousDirKeypadInputs.push_back(possiblePreviousDirKeypadInput2);
             }
 
-            transitions.emplace(toValue, possiblePreviousRobotInputs);
+            transitions.emplace(toValue, possiblePreviousDirKeypadInputs);
         }
 
         numericKeypadTransitions.emplace(fromValue, transitions);
@@ -94,93 +122,114 @@ int solutionPart1(const char* inputPath) {
             { '^', { 0, 1 } }, { 'A', { 0, 2 } }
     };
 
-    std::unordered_map<char, std::unordered_map<char, std::vector<std::string>>> directionalKeypadTransitions;
+    KeypadTransitionTable directionalKeypadTransitions;
     for (const auto& [fromValue, fromPosition] : directionalKeypad) {
         std::unordered_map<char, std::vector<std::string>> transitions;
         for (const auto& [toValue, toPosition] : directionalKeypad) {
-            std::vector<std::string> possiblePreviousRobotInputs;
-
             Position diff{ fromPosition - toPosition };
 
-            std::string possiblePrevInput1;
-            if (diff.x >= 0) {
-                possiblePrevInput1 += std::string(diff.x, '^');
-            } else {
-                possiblePrevInput1 += std::string(-diff.x, 'v');
-            }
-            if (diff.y >= 0) {
-                possiblePrevInput1 += std::string(diff.y, '<');
-            } else {
-                possiblePrevInput1 += std::string(-diff.y, '>');
-            }
-            possiblePrevInput1 += 'A';
+            std::string possiblePreviousDirKeypadInput1{
+                std::string(std::abs(diff.x), ((diff.x >= 0) ? ('^') : ('v'))) +
+                std::string(std::abs(diff.y), ((diff.y >= 0) ? ('<') : ('>')))
+            };
+            std::string possiblePreviousDirKeypadInput2{
+                possiblePreviousDirKeypadInput1.rbegin(),
+                possiblePreviousDirKeypadInput1.rend()
+            };
 
-            std::string possiblePrevInput2;
-            if (diff.y >= 0) {
-                possiblePrevInput2 += std::string(diff.y, '<');
-            } else {
-                possiblePrevInput2 += std::string(-diff.y, '>');
-            }
-            if (diff.x >= 0) {
-                possiblePrevInput2 += std::string(diff.x, '^');
-            } else {
-                possiblePrevInput2 += std::string(-diff.x, 'v');
-            }
-            possiblePrevInput2 += 'A';
+            possiblePreviousDirKeypadInput1 += 'A';
+            possiblePreviousDirKeypadInput2 += 'A';
 
+            std::vector<std::string> possiblePreviousDirKeypadInputs;
             if (diff.x == 0 || diff.y == 0 || (diff.x > 0 && fromPosition.y == 0 && toPosition.x == 0)) {
-                possiblePreviousRobotInputs.push_back(possiblePrevInput2);
+                possiblePreviousDirKeypadInputs.push_back(possiblePreviousDirKeypadInput2);
             } else if (diff.x < 0 && fromPosition.x == 0 && toPosition.y == 0) {
-                possiblePreviousRobotInputs.push_back(possiblePrevInput1);
+                possiblePreviousDirKeypadInputs.push_back(possiblePreviousDirKeypadInput1);
             } else {
-                possiblePreviousRobotInputs.push_back(possiblePrevInput1);
-                possiblePreviousRobotInputs.push_back(possiblePrevInput2);
+                possiblePreviousDirKeypadInputs.push_back(possiblePreviousDirKeypadInput1);
+                possiblePreviousDirKeypadInputs.push_back(possiblePreviousDirKeypadInput2);
             }
 
-            transitions.emplace(toValue, possiblePreviousRobotInputs);
+            transitions.emplace(toValue, possiblePreviousDirKeypadInputs);
         }
 
         directionalKeypadTransitions.emplace(fromValue, transitions);
     }
 
-    int sumOfComplexities{};
 
-    for (std::string line; std::getline(input, line); ) {
-        char currentKeypadPosition{ 'A' };
+    std::unordered_map<std::pair<std::string, int>, long long> inputCache;
 
-        int sequenceLength{};
-
-        for (char numPadButton : line) {
-            int minSequenceLength{ std::numeric_limits<int>::max() };
-
-            for (
-                const std::string& possibleTransitionsRobot1 :
-                    numericKeypadTransitions.at(currentKeypadPosition).at(numPadButton)
-            ) {
-                int humanInputLength{};
-
-                char current1stDirKeypadPosition{ 'A' };
-                for (char dirPadButtonRobot1 : possibleTransitionsRobot1) {
-                    char current2ndDirKeypadPosition{ 'A' };
-                    for (
-                        char dirPadButtonRobot2 :
-                            directionalKeypadTransitions.at(current1stDirKeypadPosition).at(dirPadButtonRobot1).front()
-                    ) {
-                        humanInputLength += directionalKeypadTransitions.at(current2ndDirKeypadPosition).at(dirPadButtonRobot2).front().length();
-
-                        current2ndDirKeypadPosition = dirPadButtonRobot2;
-                    }
-
-                    current1stDirKeypadPosition = dirPadButtonRobot1;
-                }
-
-                if (humanInputLength < minSequenceLength) {
-                    minSequenceLength = humanInputLength;
-                }
+    for (auto& [fromValue, transitions] : numericKeypadTransitions) {
+        for (auto& [toValue, possiblePreviousDirKeypadInputs] : transitions) {
+            if (possiblePreviousDirKeypadInputs.size() == 1) {
+                continue;
             }
 
-            sequenceLength += minSequenceLength;
-            currentKeypadPosition = numPadButton;
+            if (
+                countNumOfHumanButtonPresses(
+                    directionalKeypadTransitions,
+                    possiblePreviousDirKeypadInputs.front(),
+                    2,
+                    inputCache
+                )
+                >
+                countNumOfHumanButtonPresses(
+                    directionalKeypadTransitions,
+                    possiblePreviousDirKeypadInputs.back(),
+                    2,
+                    inputCache
+                )
+            ) {
+                possiblePreviousDirKeypadInputs.erase(possiblePreviousDirKeypadInputs.begin());
+            } else {
+                possiblePreviousDirKeypadInputs.pop_back();
+            }
+        }
+    }
+
+    for (auto& [fromValue, transitions] : directionalKeypadTransitions) {
+        for (auto& [toValue, possiblePreviousDirKeypadInputs] : transitions) {
+            if (possiblePreviousDirKeypadInputs.size() == 1) {
+                continue;
+            }
+
+            if (
+                countNumOfHumanButtonPresses(
+                    directionalKeypadTransitions,
+                    possiblePreviousDirKeypadInputs.front(),
+                    2,
+                    inputCache
+                )
+                >
+                countNumOfHumanButtonPresses(
+                    directionalKeypadTransitions,
+                    possiblePreviousDirKeypadInputs.back(),
+                    2,
+                    inputCache
+                )
+            ) {
+                possiblePreviousDirKeypadInputs.erase(possiblePreviousDirKeypadInputs.begin());
+            } else {
+                possiblePreviousDirKeypadInputs.pop_back();
+            }
+        }
+    }
+
+
+    long long sumOfComplexities{};
+    for (std::string line; std::getline(input, line); ) {
+        long long sequenceLength{};
+        char currentKeypadPosition{ 'A' };
+
+        for (char numKeypadButton : line) {
+            sequenceLength += countNumOfHumanButtonPresses(
+                    directionalKeypadTransitions,
+                    numericKeypadTransitions.at(currentKeypadPosition).at(numKeypadButton).front(),
+                    numOfRobotsUsingDirKeypads,
+                    inputCache
+            );
+
+            currentKeypadPosition = numKeypadButton;
         }
 
         line.pop_back();
@@ -188,13 +237,6 @@ int solutionPart1(const char* inputPath) {
     }
 
     return sumOfComplexities;
-}
-
-
-int solutionPart2(const char* inputPath) {
-    std::ifstream input(inputPath);
-
-    return 0;
 }
 
 
@@ -211,14 +253,14 @@ double measureTime(const std::function<void()>& func, int numOfRuns) {
 
 int main() {
     std::cout << "Test inputs:" << std::endl;
-    std::cout << "\tPart 1: " << solutionPart1(TEST_INPUT_PART1_PATH) << std::endl;
-//    std::cout << "\tPart 2: " << solutionPart2(TEST_INPUT_PART2_PATH) << std::endl;
+    std::cout << "\tPart 1: " << solution(TEST_INPUT_PART1_PATH, 2) << std::endl;
+    std::cout << "\tPart 2: " << solution(TEST_INPUT_PART2_PATH, 25) << std::endl;
     std::cout << "My input:" << std::endl;
-    std::cout << "\tPart 1: " << solutionPart1(MY_INPUT_PATH) << std::endl;
-//    std::cout << "\tPart 2: " << solutionPart2(MY_INPUT_PATH) << std::endl;
+    std::cout << "\tPart 1: " << solution(MY_INPUT_PATH, 2) << std::endl;
+    std::cout << "\tPart 2: " << solution(MY_INPUT_PATH, 25) << std::endl;
     std::cout << "My input runtime [ms]:" << std::endl;
-    std::cout << "\tPart 1: " << measureTime([](){ solutionPart1(MY_INPUT_PATH); }, 1000) << std::endl;
-//    std::cout << "\tPart 2: " << measureTime([](){ solutionPart2(MY_INPUT_PATH); }, 1000) << std::endl;
+    std::cout << "\tPart 1: " << measureTime([](){ solution(MY_INPUT_PATH, 2); }, 1000) << std::endl;
+    std::cout << "\tPart 2: " << measureTime([](){ solution(MY_INPUT_PATH, 25); }, 1000) << std::endl;
 
     return 0;
 }
